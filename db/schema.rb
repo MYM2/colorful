0607fc27f834226112f82106c5a1a5209311f185
naomichi999 <naomichi999@gmail.com>
2019-09-09 15:08:13 +0900

    [add]Favoriteモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_060047) do
+ActiveRecord::Schema.define(version: 2019_09_09_060728) do
 
   create_table "deliveries", force: :cascade do |t|
     t.integer "end_user_id"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2019_09_09_060047) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "end_user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "inquiries", force: :cascade do |t|
