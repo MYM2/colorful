82f248c3d11343c9685fafe215a88e8f4c13424a
naomichi999 <naomichi999@gmail.com>
2019-09-09 15:23:13 +0900

    [add]songモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_062031) do
+ActiveRecord::Schema.define(version: 2019_09_09_062231) do
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name"
@@ -114,6 +114,14 @@ ActiveRecord::Schema.define(version: 2019_09_09_062031) do
     t.integer "end_user_id"
     t.integer "product_id"
     t.text "review_content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.integer "disc_id"
+    t.integer "track_number"
+    t.string "song_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
