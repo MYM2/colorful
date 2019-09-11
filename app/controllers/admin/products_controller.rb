6ea231d89df3d8477cb1278967eb1ce4ffe55e3c
birnamwood <birnamwood19@gmail.com>
2019-09-11 20:34:32 +0900

    [add]商品追加機能作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,14 +9,29 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @disc = @product.discs.build
+    @songs = @disc.songs.build
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to new_admin_product_path
+      end
   end
 
   def update
   end
 
-  def create
-  end
-
   def destroy
   end
+
+  private
+    def product_params
+    params.require(:product).permit(:genre_id, :artist_id, :product_name, :price, :label_id,
+                                    :stopped, :jacket_image_id,
+                                    disc_attributes: [:id, :product_id, :disc_number, :_destroy,
+                                    song_attributes: [:id, :dick_id, :track_number, :song_name, :_destroy]])
+    end
 end
