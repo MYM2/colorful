ffdfe3722ef481b570f2809278c7a6b8994a3e22
birnamwood <birnamwood19@gmail.com>
2019-09-13 03:26:59 +0900

    [add]トップにスライドショー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,13 @@
 //= require_tree .
 
 
+$(function() {
+  $('.slick').slick(
+    {
+        prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+        nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+        autoplay:true,
+        dots:true,
+    }
+  );
+});
