2c7ca94738a2849d8480eb65276e6c0dbc783fb6
birnamwood <birnamwood19@gmail.com>
2019-09-13 02:20:01 +0900

    [add]商品登録、編集、閲覧機能作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,27 +1,42 @@
 class Admin::ProductsController < ApplicationController
   def index
+    @products = Product.includes([:artist, :genre]).page(params[:page]).reverse_order.per(20)
   end
 
   def show
+    @product = Product.find(params[:id])
+    @discs = @product.discs.includes(:songs)
+    @artist = @product.artist
+    @genre = @product.genre
+    @label = @product.label
   end
 
   def edit
+    @product_new = Product.new
+    @disc = @product_new.discs.build
+    @song = @disc.songs.build
+
+    @product = Product.find(params[:id])
   end
 
   def new
-    @product = Product.new
-    @disc = @product.discs.build
-    @songs = @disc.songs.build
+    @product_new = Product.new
+    @disc_new = @product_new.discs.build
+    @song_new = @disc_new.songs.build
   end
 
   def create
     @product = Product.new(product_params)
       if @product.save
-        redirect_to new_admin_product_path
+        redirect_to admin_product_path(@product)
       end
   end
 
   def update
+    @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to admin_product_path(@product)
+      end
   end
 
   def destroy
@@ -29,9 +44,9 @@ class Admin::ProductsController < ApplicationController
 
   private
     def product_params
-    params.require(:product).permit(:genre_id, :artist_id, :product_name, :price, :label_id,
-                                    :stopped, :jacket_image_id,
-                                    disc_attributes: [:id, :product_id, :disc_number, :_destroy,
-                                    song_attributes: [:id, :dick_id, :track_number, :song_name, :_destroy]])
+    params.require(:product).permit(:genre_id, :artist_id, :product_name, :price, :label_id, 
+                                    :stopped, :jacket_image,
+                                    discs_attributes: [:id, :product_id, :disc_number, :_destroy,
+                                    songs_attributes: [:id, :dick_id, :track_number, :song_name, :_destroy]])
     end
 end
