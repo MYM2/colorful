20304b06e7acc6826fddc8798ef3c417f01e09d4
naomichi999 <naomichi999@gmail.com>
2019-09-14 16:45:47 +0900

    [fix]マイページ→商品一覧のルーティングエラー

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -8,5 +8,13 @@ class EndUsersController < ApplicationController
   end
 
   def update
+  	 @end_user = EndUser.find(params[:id])
+  	 @end_user.update(end_user_params)
+  	 redirect_to end_user_path(@end_user.id)
   end
+
+    protected
+    def end_user_params
+      params.require(:end_user).permit(:lastname_kanji, :firstname_kanji, :lastname_kana, :firstname_kana, :zipcode, :address, :phone_number, :email)
+    end
 end
