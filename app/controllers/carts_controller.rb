f57b9a9814f06e6f7827d34263ac10069a23b092
birnamwood <birnamwood19@gmail.com>
2019-09-14 21:49:22 +0900

    [fix]カートに入れる機能の修正（モデルのバリデーション修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,8 +10,11 @@ class CartsController < ApplicationController
   def create
     # binding.pry
     @cart = Cart.new(product_id: params[:product_id], product_qty: params[:product_qty], end_user_id: current_end_user.id)
-    binding.pry
+    @product = Product.find(params[:product_id])
       if @cart.save
+        redirect_to product_path(@product)
+      else
+        redirect_to products_path
       end
   end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
 	belongs_to :product
 	belongs_to :end_user
-	validates :product_qty, :numericality => { :greater_than_or_equal_to => 5 }
+  validates :product_qty, :numericality => { :greater_than_or_equal_to => 1 }
 end
