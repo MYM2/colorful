03626ca9f0e78fea475aea5455e928fa5ede39a1
birnamwood <birnamwood19@gmail.com>
2019-09-19 17:58:22 +0900

    [update]注文内容の総合計金額欄を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,19 +3,25 @@ class OrdersController < ApplicationController
   end
 
   def new
+    # オーダー関係オブジェクト
     @order = Order.new
     @order_content = OrderContent.new
 
+    # カートの中身
     @end_user = current_end_user
     @carts = @end_user.carts.includes(:product)
+    @freight = 500
 
+    # カート内合計金額
     @carts_total = @end_user.carts.includes(:product)
     @total_price = 0
     @carts_total.each do |cart|
       @total_price += cart.product.price * cart.product_qty
     end
+
   end
 
   def create
+    puts "order-create"
   end
 end
