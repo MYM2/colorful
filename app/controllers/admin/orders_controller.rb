fd3b03591c55f943354a930dad2d6dd53efd812d
birnamwood <birnamwood19@gmail.com>
2019-09-22 20:24:18 +0900

    [add]出荷ステータス変更機能作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,8 +3,18 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
+    @orders = @end_user.orders.includes(:order_contents).page(params[:page]).reverse_order.per(5)
   end
 
   def update
+    @order = Order.find(order_params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(params[:id])
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:id, :arrival_status)
+    end
 end
