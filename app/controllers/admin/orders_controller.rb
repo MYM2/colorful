8d75a64cb30a7ec0328c65d4a46e82db665dcec9
birnamwood <birnamwood19@gmail.com>
2019-09-23 13:43:47 +0900

    [update]管理者側購入履歴一覧表示作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all.includes(:order_contents).page(params[:page]).reverse_order.per(5)
   end
 
   def show
@@ -8,13 +9,18 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(order_params[:id])
+    @order = Order.find(params[:id])
     @order.update(order_params)
-    redirect_to admin_order_path(params[:id])
+    # ユーザー購入一覧／全体購入一覧の来たページに戻る
+    if order_params[:end_user_id].nil?
+      redirect_to admin_orders_path
+    else
+      redirect_to admin_order_path(order_params[:end_user_id])
+    end
   end
 
   private
     def order_params
-      params.require(:order).permit(:id, :arrival_status)
+      params.require(:order).permit(:id, :arrival_status, :end_user_id)
     end
 end
