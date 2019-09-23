b6c81a1afba223e9c28c3e2a76adfe0b8b2aaa7a
birnamwood <birnamwood19@gmail.com>
2019-09-23 15:23:40 +0900

    [fix]商品の税込み値段計算式の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,11 +18,12 @@ class OrdersController < ApplicationController
 
     # カート内合計金額
     @carts_total = @end_user.carts.includes(:product)
-    @total_price = 0
-    @carts_total.each do |cart|
-      @total_price += cart.product.price * cart.product_qty
-    end
-    @total_price_in_tax = (@total_price * Colorful::Application.config.InTax).floor
+      @total_price = 0
+      @total_price_in_tax = 0
+      @carts_total.each do |cart|
+        @total_price += cart.product.price * cart.product_qty
+        @total_price_in_tax += (cart.product.price * Colorful::Application.config.InTax).floor * cart.product_qty
+       end
 
 
   end
@@ -32,20 +33,22 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @end_user = current_end_user
     @order.end_user_id = current_end_user.id
-      if @order.save
+      # if @order.save
         flash[:success] = "注文が完了しました。"
+        # カート内を削除
         redirect_to products_path
-      else
+      # else
         flash[:danger] = "注文に失敗しました。"
         redirect_to products_path
-      end
+      # end
 
   end
 
   private
     def order_params
-      params.require(:order).permit(:deliveries_address, :payment_method, :subtotal_ex_tax, :subtotal_in_tax, :freight, :arrival_status,
+      params.require(:order).permit(:deliveries_address, :payment_method, :subtotal_ex_tax, :subtotal_in_tax, :freight, :arrival_status, :cart,
                                     order_contents_attributes: [:id, :product_id, :product_qty, :price_sum_ex_tax, :price_sum_in_tax])
     end
+
 
 end
