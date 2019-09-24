af9c6aeaf7cb91517ccf6d56c80bc5a291af91c9
吉松拓巳 <matty@mattymacbookpro.local>
2019-09-24 12:17:06 +0900

    [add]ランキング画面の作成

--- app/controllers/admin/rankings_controller.rb
+++ app/controllers/admin/rankings_controller.rb
@@ -1,6 +1,5 @@
 class Admin::RankingsController < ApplicationController
   def index
-  	@product = Product.find(OrderContent.group(:product_id).order('sum(price_sum_ex_tax) desc').pluck(:product_id))
-
+  	@product = Product.find(OrderContent.group(:product_id).order('sum(price_sum_ex_tax) desc').limit(30).pluck(:product_id))
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,16 @@ def set_search
   #@search = Product.search(params[:q])
   @search = Product.ransack(params[:q]) #ransackメソッド推奨
   @search_products = @search.result.page(params[:page])
-  
+
 end
-
-
+module Colorful
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja #追加
+  end
+end
 protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname_kanji, :firstname_kanji, :lastname_kana, :firstname_kana, :phone_number])
